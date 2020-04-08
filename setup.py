60847badafc76d72370d40a408ecd1876736367b
faulknerrainford <kajos.falcon@googlemail.com>
2020-04-08 12:43:15 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=['SPmodelling'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General License",
